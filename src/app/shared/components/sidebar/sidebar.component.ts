aa20056a3fdc03a0c204789d86244e85a06ae2e0
Alonso <marenas@itsmarts.com.mx>
2024-02-17 09:24:15 -0600

    Primer commit

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent{
+  //! Inyectar el servicio en el sidebar
+  //! Duplicar el elemeto del html dependiendo de cuantas entradas tenga
+  //! gifservice
+  //? Propiedad publica ue el html pueda ver
+  // private
+
+  constructor(private gifsService: GifsService) {}
+
+  get tags(): string[] {
+    const tagList = this.gifsService.tagsHistory;
+    // console.log(tagList)
+    return tagList;
+  }
+
+}
